3e6d53514ac8ae32650de9423fa74213efff778a
Andrea Borghi <andrea.borghi@camptocamp.com>
2024-01-22 08:10:37 +0100

    Add local file parser

--- alertswisscap/apiclient/client.py
+++ alertswisscap/apiclient/client.py
@@ -3,23 +3,36 @@ import json
 import xmltodict
 from capparselib.parsers import CAPParser
 
-
+# DEFAULT_CAP_URL = "https://alertswiss.poatest.ch/alertswiss/v2/alerts?scope=Public"
+# DEFAULT_CAP_URL = "https://alertswiss.poatest.ch/alertswiss/v2/alerts?scope=Restricted"
+# DEFAULT_CAP_URL = "https://alertswiss.poatest.ch/alertswiss/v2/alerts?scope=All"
+DEFAULT_CAP_URL = "https://alertswiss.poatest.ch/alertswiss/v1/alerts"
 
 class CAPClient:
-    def __init__(self, url="https://alertswiss.poatest.ch/alertswiss/v1/alerts"):
+    def __init__(self, url=DEFAULT_CAP_URL):
         self.url = url
         
+    def strip_url(self):
+        if self.url.startswith('file://'):
+            return self.url[7:]
+        else:
+            return self.url
+    
+    def _get_raw_alerts(self):
+        if self.url.startswith('http'):
+            response = requests.get(self.url)
+            if response.status_code != 200:
+                raise Exception('Error while getting alerts')
+            return json.loads(response.content)['body']['alerts']
+        elif self.url.startswith('file'):
+            with open(self.strip_url(), 'r') as f:
+                return json.loads(f.read())['body']['alerts']
+        
     def get_parsed_alerts(self):
-        response = requests.get(self.url)
-        if response.status_code != 200:
-            raise Exception('Error while getting alerts')
-        return self._parse_alerts(json.loads(response.content)['body']['alerts'])
+        return self._parse_alerts(self._get_raw_alerts())
     
     def get_alerts(self):
-        response = requests.get(self.url)
-        if response.status_code != 200:
-            raise Exception('Error while getting alerts')
-        return json.loads(response.content)['body']['alerts']
+        return self._get_raw_alerts()
        
     def _parse_alerts(self, alerts):
         parsed_alerts = []
--- alertswisscap/loader.py
+++ alertswisscap/loader.py
@@ -1,24 +1,36 @@
-from apiclient.client import CAPClient
+from apiclient.client import CAPClient, DEFAULT_CAP_URL
 from model.geometries import AlertSwissCapGeometryMultiPolygon
+import argparse as ap
 
-client = CAPClient()
-alerts = client.get_parsed_alerts()
 
-cap_polygons = {}
+def parser():
+    parser = ap.ArgumentParser(description='AlertSwiss CAP Client')
+    parser.add_argument('-u', '--url', default=DEFAULT_CAP_URL, help='url of api or input file', required=False)
+    return parser.parse_args()
 
-cap_polygons['polygons'] = alerts[0]['content'][0]['cap_info'][1]['cap_area'][0]['polygons']
-cap_polygons['exclude_polygons'] = []
-for geocode in  alerts[0]['content'][0]['cap_info'][1]['cap_area'][0]['geocodes']:
-        if geocode['valueName'] == 'ALERTSWISS_EXCLUDE_POLYGON':
-            cap_polygons['exclude_polygons'].append(geocode['value'])
+def main():
+    args = parser()
+    client = CAPClient(url=args.url)
+    alerts = client.get_parsed_alerts()
 
-#print(cap_polygons)
+    cap_polygons = {}
 
-# note to self
-# in alerts[nb_alert]['content'][0]['cap_info'][np_cap_info]['cap_area'][0]['polygons']
-# sometimes there is no 'cap_area'
-# possibly sometimes there is no 'polygons'
-# nb_cap_info is equal to the number of languages, but all languages have the same exact 'cap_area' and 'polygons', so we can just take the first one
+    cap_polygons['polygons'] = alerts[0]['content'][0]['cap_info'][1]['cap_area'][0]['polygons']
+    cap_polygons['exclude_polygons'] = []
+    for geocode in  alerts[0]['content'][0]['cap_info'][1]['cap_area'][0]['geocodes']:
+            if geocode['valueName'] == 'ALERTSWISS_EXCLUDE_POLYGON':
+                cap_polygons['exclude_polygons'].append(geocode['value'])
 
-mp = AlertSwissCapGeometryMultiPolygon(cap_polygons)
-print(mp._multiPolygon)
+    #print(cap_polygons)
+
+    # note to self
+    # in alerts[nb_alert]['content'][0]['cap_info'][np_cap_info]['cap_area'][0]['polygons']
+    # sometimes there is no 'cap_area'
+    # possibly sometimes there is no 'polygons'
+    # nb_cap_info is equal to the number of languages, but all languages have the same exact 'cap_area' and 'polygons', so we can just take the first one
+
+    mp = AlertSwissCapGeometryMultiPolygon(cap_polygons)
+    print(mp._multiPolygon)
+
+if __name__ == '__main__':
+    main()
